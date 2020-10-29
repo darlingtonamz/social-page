5afd883b11dd59d2342e6bf76b38ba50dee686cf
Amanze Ogbonna <darlingtonamz@gmail.com>
2020-10-29 20:34:33 +0100

    Add router to app, and implement route switching for SiteLink through the address-bar

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,52 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          :src="appConfig.siteIconUrl || 'https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png'"
-          transition="scale-transition"
-          width="40"
-        />
-        <span class="site-title">
-          {{appConfig.siteName}}
-        </span>
-      </div>
+  <div id="app">
+    <v-app>
+      <v-app-bar
+        app
+        color="primary"
+        dark
+      >
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            :src="appConfig.siteIconUrl || 'https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png'"
+            transition="scale-transition"
+            width="40"
+          />
+          <span class="site-title">
+            {{appConfig.siteName}}
+          </span>
+        </div>
 
-      <v-spacer></v-spacer>
-    </v-app-bar>
+        <v-spacer></v-spacer>
+      </v-app-bar>
 
-    <v-main
-      :style="{ background: (appConfig.background || '#e3f2fd') }"
-    >
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+      <v-main
+        :style="{ background: (appConfig.background || '#e3f2fd') }"
+      >
+    <router-view/>
+        <!-- <HelloWorld/> -->
+      </v-main>
+    </v-app>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
 import appConfig from './configs/appConfig';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  data: () => ({}),
   computed: {
     appConfig: () => appConfig,
   }
 };
 </script>
 <style lang="scss">
+#app {
+  font-family: Roboto;
+}
+
 .site-title {
   font-size: 2em;
   font-weight: 500;
--- src/configs/socialLinks.js
+++ src/configs/socialLinks.js
@@ -1,5 +1,6 @@
 export default [
   {
+    id: 'person-page',
     url: 'https://www.amanze.me',
     title: 'Personal Website',
     iconUrl: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
@@ -10,6 +11,7 @@ export default [
     }
   },
   {
+    id: 'yt-page',
     url: 'https://www.youtube.com/channel/UCa475eqLbfWj2wRbdhQU6fA',
     title: 'Youtube',
     icon: 'fab fa-youtube',
@@ -20,6 +22,7 @@ export default [
     }
   },
   {
+    id: 'fb-page',
     url: 'https://www.facebook.com/africoder',
     title: 'Facebook difudifudi',
     icon: 'fab fa-facebook',
@@ -30,9 +33,29 @@ export default [
     }
   },
   {
+    id: 'more-social',
     title: 'More SocialLinks',
     subTitle: 'This is a collection of SocialLinks',
     children: [
+      {
+        id: 'linkseption',
+        title: 'Linkseption',
+        subTitle: 'This is a collection of SocialLinks',
+        children: [
+          {
+            id: 'linkseption',
+            title: 'Linkseption',
+            subTitle: 'This is a collection of SocialLinks',
+            children: [
+              {
+                id: 'linkseption',
+                title: 'Linkseption',
+                subTitle: 'This is a collection of SocialLinks',
+              },
+            ]
+          },
+        ]
+      },
       {
         title: 'Broken Link',
         iconUrl: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
